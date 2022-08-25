a4f3450b7c2a08502eefd8500dc8e66c1ac614fd
ysa-v <97559097+ysa-v@users.noreply.github.com>
2022-08-24 20:37:41 -0500

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MenuStudio
+{
+    public class Menu
+    {
+        Dictionary<double,string> ItemsPriceList { get; set; }
+        List<MenuItem> FullMenu { get; set; }
+        List<string> CategoryOrder { get; set; }
+        DateTime MenuLastUpdated { get; set; }
+        int NumberOfNewItems { get; set; }
+
+        public Menu(List<MenuItem> fullMenu)
+        {
+            FullMenu = fullMenu;
+        }
+
+        public void AddMenuItem(MenuItem item)
+        {
+            if (FullMenu.Contains(item))
+            {
+                return;
+            } else
+            {
+                FullMenu.Add(item);
+            }
+        }
+
+        public void RemoveMenuItem(MenuItem item)
+        {
+            FullMenu.Remove(item);
+        }
+
+        public void IsNew (List<MenuItem> FullMenu)
+        {
+            List<MenuItem> newItems = FullMenu.Where(x => x.MarkedAsNew == true).ToList();
+        }
+
+        public void MenuLastUpdatedMethod ()
+        {
+            Console.WriteLine("Menu Last Updated: " + MenuLastUpdated);
+        }
+    }
+}
+
---
+++ MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic
+namespace MenuStudio
+{
+    public class MenuItem
+    {
+        public double Price { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool MarkedAsNew { get; set; }
+        public double Popularity { get; set; }
+        public DateTime ItemCreated { get; set; }
+        public DateTime ItemUpdated { get; set; }
+
+        public MenuItem (double price, string title, string description, bool markedAsNew)
+        {
+            Price = price;
+            Title = title;
+            Description = description;
+            MarkedAsNew = markedAsNew;
+        }
+
+        public MenuItem ()
+    }
+}
+
